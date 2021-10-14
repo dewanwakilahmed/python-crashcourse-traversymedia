470f8838aba81b1e40b49559a02fd7169cfa7207
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-10-14 23:22:46 +0600

    Variables
    Added variables.py

---
+++ variables.py
@@ -0,0 +1,35 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+
+x = 1               # int
+y = 2.5             # float
+name = 'John'       # str
+is_cool = True      # bool
+
+print(x, y, name, is_cool)
+
+# Basic Math
+a = x + y
+
+print(a)
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(x), type(y))
+print(z, type(z))
